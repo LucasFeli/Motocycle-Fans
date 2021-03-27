c9fad5768d33577332e47cd643720cef915b3be7
LucasFeli <felipe298@gmail.com>
2021-03-27 09:54:39 +0100

    modificacion data

--- seed/data.js
+++ seed/data.js
@@ -11,7 +11,7 @@ module.exports = [
     marca: "Suzuki A 50 P/D",
     modelo : 1979,
     motor: "Dos tiempos, válvula de piston, monocilindrico ",
-    image:"http://suprememotos.com/uploads/postfotos/suzuki-a50p-ap50-blue-uk-model-sixteener-special-sports-moped-1.JPG",
+    image:"https://s1.cdn.autoevolution.com/images/moto_gallery/SUZUKI-A50-14461_1.jpg",
     description: "La nueva Suzuki era una máquina profesional para mirar con una gran postura y actitud de moto. La usabilidad también fue una delicia con un sistema de autolubricación mucho antes de su tiempo, la bomba fue impulsada por el engranaje inactivo de arranque que a su vez tomó su impulso desde el eje de salida de la caja de engranajes, esto resultó en el eje de arranque no funciona cuando se aprieta el embrague, pero es un pequeño precio a pagar por la facilidad que se encuentra en el momento del llenado. ",
 },
 {
@@ -21,5 +21,11 @@ module.exports = [
     image:"https://www.ktm.com/ktmgroup-storage/PHO_BIKE_90_RE_300-EXC-TPI-Erzberg-MY21-90-Right_%23SALL_%23AEPI_%23V1.png",
     description: "La KTM 300 EXC ha estado a la vanguardia del loco ritmo de desarrollo que es el Enduro Extremo. Este ágil y ligera moto offroad no teme a ningún obstáculo y se convierte en la máquina definitiva para conquistar la legendaria Iron Giant. La edición limitada especial ERZBERGRODEO está cargada de piezas KTM PowerParts y luce un exclusivo kit de gráficos para conmemorar este evento tan especial.  ",
 },
+{
+    marca: "Suzuki GSX-R600  ",
+    modelo : 2018,
+    motor: "Motor de 599 cc y 4 cilindros con inyección de combustible que proporciona una gran potencia desde el ralentí hasta la línea roja",
+    image:"https://www.totalmotorcycle.com/wp-content/uploads/2017/11/2018-Suzuki-GSX-R600b-1024x682.jpg ",
+    description: "La Suzuki GSX-R 600 2018 es una motocicleta deportiva líder en su clase, digna de su herencia GSX-R ganadora de carreras. Ya sea que este curveando por tu zona favorita o dominando la pista de carreras, ofrece un rendimiento sin igual. ",},
 
 ];
